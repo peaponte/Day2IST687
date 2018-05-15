c45ea2b9897392a0178bffce622072cf6a239db6
alexbruso <39306864+alexbruso@users.noreply.github.com>
2018-05-15 13:40:35 -0400

    moving NPS_Part_D.R to NPS_Function.R

---
+++ NPS_Function.R
@@ -0,0 +1,23 @@
+#####################################
+##### Do NOT delete this block #####
+setwd(Sys.getenv('PROJECT_HOME'))
+####################################
+
+####################################
+## write code to read input csv into data frame
+df <- read.csv('cleandata.csv')
+####################################
+
+## start writing your R code from here
+
+## end your R code and logic 
+
+####################################
+##### write output file ############
+write.csv(df, file = 'data_7865.csv')
+####################################
+
+
+
+
+
