989d040e8eb96918eb5242714c62c34f26a0d7e7
alexbruso <39306864+alexbruso@users.noreply.github.com>
2018-05-15 16:20:20 -0400

    saving NPS_Function_1.R

--- NPS_Function_1.R
+++ NPS_Function_1.R
@@ -5,11 +5,28 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-df <- read.csv('LTR_Data.csv')
+dfClean <- read.csv('LTR_Data.csv')
 ####################################
 
 ## start writing your R code from here
-
+NPSfunction <- function(dfClean)
+{
+  #Promoters are numbers of 9 or 10
+  Promoters <- sum(dfClean$Likelihood_Recommend_H > 8)
+  Promoters
+  #Detractors are numbers of 6 or less
+  Detractors <- sum(dfClean$Likelihood_Recommend_H<7)
+  
+  #Find percent promotors
+  PercentPromoters <- Promoters/length(dfClean$Likelihood_Recommend_H)
+  
+  #Find percent detractors
+  PercentDetractors <- Detractors/length(dfClean$Likelihood_Recommend_H)
+  
+  #NPS calculation then round number and convert to percent
+  NPS <- PercentPromoters - PercentDetractors
+  format(round(NPS*100,2))
+}
 ## end your R code and logic 
 
 ####################################
