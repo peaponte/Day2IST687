d83fc2081f0496db7789e5ebd7436e37429a7c44
alexbruso <39306864+alexbruso@users.noreply.github.com>
2018-05-17 09:18:24 -0400

    saving clean.R

--- clean.R
+++ clean.R
@@ -5,16 +5,13 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-df <- read.csv('raw_data.csv')
+df <- read.csv('C:/Users/Alex/stigmergy/Day3/raw_data.csv')
 ####################################
 
 ## start writing your R code from here
 is.na(df)
 dfClean <- df[!is.na(df$Likelihood_Recommend_H), ]
 dfClean
-dfVal <- is.na(df$Likelihood_Recommend_H)
-dfVal
-sum(dfVal)
 ## end your R code and logic 
 
 ####################################
